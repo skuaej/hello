a55514a939d6dbee20cd56d7b7a994b16b177f3e
skuaej <sumankumar821310@gmail.com>
2025-08-12 02:33:47 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,20 +1,36 @@
-import os
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update
+import logging
 
-BOT_TOKEN = os.environ.get("8171830754:AAFHMKLVn5XjRM-Sm11vm5q9VJ37iyBMeaA")
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-if not BOT_TOKEN:
-    raise RuntimeError("Please set TELEGRAM_BOT_TOKEN environment variable.")
+logger = logging.getLogger(__name__)
 
-def start(update, context):
-    update.message.reply_text("Hi")
+def start(update: Update, context: CallbackContext) -> None:
+    """Sends a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-def main():
-    updater = Updater(BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+def main() -> None:
+    """Start the bot."""
+    # Replace 'YOUR_TOKEN' with your actual bot token
+    updater = Updater("8171830754:AAFHMKLVn5XjRM-Sm11vm5q9VJ37iyBMeaA", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
